a9c31d1fb8841081a3937032f44728b64077c296
lyla-says-hi <lylaknifong@gmail.com>
2024-12-08 13:13:16 -0800

    Added characters table

--- week7/answers.sql
+++ week7/answers.sql
@@ -8,3 +8,15 @@ CREATE TABLE players (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE characters (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  player_id INT UNSIGNED NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  level INT UNSIGNED NOT NULL,
+  CONSTRAINT characters_fk_players
+    FOREIGN KEY (player_id)
+    REFERENCES players (player_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
