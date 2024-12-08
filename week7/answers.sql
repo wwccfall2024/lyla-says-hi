2a93567bd47b43fe635b9287f77045b4bb3e79a0
lyla-says-hi <lylaknifong@gmail.com>
2024-12-08 14:05:07 -0800

    Added remaining tables

--- week7/answers.sql
+++ week7/answers.sql
@@ -20,3 +20,84 @@ CREATE TABLE characters (
     ON UPDATE CASCADE
     ON DELETE CASCADE
 );
+
+CREATE TABLE winners (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  CONSTRAINT winners_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE character_stats (
+  character_id INT UNSIGNED PRIMARY KEY NOT NULL,
+  health INT UNSIGNED NOT NULL,
+  armor INT UNSIGNED NOT NULL,
+  CONSTRAINT character_stats_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+  team_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE team_members (
+  team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  team_id INT UNSIGNED NOT NULL,
+  character_id INT UNSIGNED NOT NULL,
+  CONSTRAINT team_members_fk_teams
+    FOREIGN KEY (team_id)
+    REFERENCES teams (team_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT team_members_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+  item_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  armor INT UNSIGNED NOT NULL,
+  damage INT UNSIGNED NOT NULL
+);
+
+CREATE TABLE inventory (
+  inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED NOT NULL,
+  item_id INT UNSIGNED NOT NULL,
+  CONSTRAINT inventory_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT inventory_fk_items
+    FOREIGN KEY (item_id)
+    REFERENCES items (item_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE equipped (
+  equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  character_id INT UNSIGNED NOT NULL,
+  item_id INT UNSIGNED NOT NULL,
+  CONSTRAINT equipped_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT equipped_fk_items
+    FOREIGN KEY (item_id)
+    REFERENCES items (item_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
